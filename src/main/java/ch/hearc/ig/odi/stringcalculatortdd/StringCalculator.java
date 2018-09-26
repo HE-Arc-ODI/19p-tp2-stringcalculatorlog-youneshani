63a19e268d1fdc0b80fa57c43149b9bafc9f85ee
dylan-sk8 <dylan-sk8@hotmail.com>
2018-09-26 12:39:24 +0200

    adding last requirements

--- src/main/java/ch/hearc/ig/odi/stringcalculatortdd/StringCalculator.java
+++ src/main/java/ch/hearc/ig/odi/stringcalculatortdd/StringCalculator.java
@@ -1,24 +1,19 @@
 package ch.hearc.ig.odi.stringcalculatortdd;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.text.StringEscapeUtils;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
 
-    // Requirement 1
-//    public static final void add(final String numbers) {
-//        String[] numbersArray = numbers.split(",");
-//        if (numbersArray.length > 2) {
-//            throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
-//        } else {
-//            for (String number : numbersArray) {
-//                Integer.parseInt(number); // If it is not a number, parseInt will throw an exception
-//            }
-//        }
-//    }
+    public StringCalculator() {
+    }
 
-    // Requirement 2 (string empty)
-//    public static final int add(final String numbers) {
+    // Requirement 1.1 (string empty)
+//    public final int add(final String numbers) {
 //        String[] numbersArray = numbers.split(",");
 //        if (numbersArray.length > 2) {
 //            throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
@@ -32,8 +27,8 @@ public class StringCalculator {
 //        return 0;
 //    }
 
-    //Requirement 3
-//    public static final int add(final String numbers) {
+    //Requirement 1.2 - 1.3 (1 or 2 numbers)
+//    public final int add(final String numbers) {
 //        int returnValue = 0;
 //        String[] numbersArray = numbers.split(",");
 //        if (numbersArray.length > 2) {
@@ -47,8 +42,8 @@ public class StringCalculator {
 //        return returnValue;
 //    }
 
-    //Requirement 4
-//    public static final int add(final String numbers) {
+    //Requirement 2
+//    public final int add(final String numbers) {
 //        int returnValue = 0;
 //        String[] numbersArray = numbers.split(",");
 //
@@ -60,8 +55,8 @@ public class StringCalculator {
 //        return returnValue;
 //    }
 
-    //Requirement 5
-//    public static final int add(final String numbers) {
+    //Requirement 3
+//    public final int add(final String numbers) {
 //        int returnValue = 0;
 //        String[] numbersArray = numbers.split(",|\n");
 //
@@ -73,19 +68,19 @@ public class StringCalculator {
 //        return returnValue;
 //    }
 
-    //Requirement 6
-//    public static int add(final String numbers) {
+    //Requirement 4
+//    public int add(final String numbers) {
 //        String delimiter = ",|\n";
-//        String numbersWithoutDelimiter = numbers;
+//        String numbersUpdated = numbers;
 //        if (numbers.startsWith("//")) {
 //            int delimiterIndex = numbers.indexOf("//") + 2;
 //            delimiter = numbers.substring(delimiterIndex, delimiterIndex + 1);
-//            numbersWithoutDelimiter = numbers.substring(numbers.indexOf("\n") + 1);
+//            numbersUpdated = numbers.substring(numbers.indexOf("\n") + 1);
 //        }
-//        return add(numbersWithoutDelimiter, delimiter);
+//        return add(numbersUpdated, delimiter);
 //    }
 //
-//    private static int add(final String numbers, final String delimiter) {
+//    private int add(final String numbers, final String delimiter) {
 //        int returnValue = 0;
 //        String[] numbersArray = numbers.split(delimiter);
 //        for (String number : numbersArray) {
@@ -96,19 +91,19 @@ public class StringCalculator {
 //        return returnValue;
 //    }
 
-    //Requirement 7
-//    public static int add(final String numbers) {
+    //Requirement 5
+//    public int add(final String numbers) {
 //        String delimiter = ",|\n";
-//        String numbersWithoutDelimiter = numbers;
+//        String numbersUpdated = numbers;
 //        if (numbers.startsWith("//")) {
 //            int delimiterIndex = numbers.indexOf("//") + 2;
 //            delimiter = numbers.substring(delimiterIndex, delimiterIndex + 1);
-//            numbersWithoutDelimiter = numbers.substring(numbers.indexOf("\n") + 1);
+//            numbersUpdated = numbers.substring(numbers.indexOf("\n") + 1);
 //        }
-//        return add(numbersWithoutDelimiter, delimiter);
+//        return add(numbersUpdated, delimiter);
 //    }
 //
-//    private static int add(final String numbers, final String delimiter) {
+//    private int add(final String numbers, final String delimiter) {
 //        int returnValue = 0;
 //        List<Integer> negativeNumbers = new ArrayList();
 //        String[] numbersArray = numbers.split(delimiter);
@@ -129,22 +124,120 @@ public class StringCalculator {
 //        return returnValue;
 //    }
 
-    //Requirement 8
-    public static int add(final String numbers) {
+    //Requirement 6
+//    public int add(final String numbers) {
+//        String delimiter = ",|\n";
+//        String numbersUpdated = numbers;
+//        if (numbers.startsWith("//")) {
+//            int delimiterIndex = numbers.indexOf("//") + 2;
+//            delimiter = numbers.substring(delimiterIndex, delimiterIndex + 1);
+//            numbersUpdated = numbers.substring(numbers.indexOf("\n") + 1);
+//        }
+//        return add(numbersUpdated, delimiter);
+//    }
+//
+//    private int add(final String numbers, final String delimiter) {
+//        int returnValue = 0;
+//        List<Integer> negativeNumbers = new ArrayList();
+//        String[] numbersArray = numbers.split(delimiter);
+//        for (String number : numbersArray) {
+//            if (!(number.trim().length() == 0)) {
+//                Integer numberInt = Integer.parseInt(number);
+//                if (numberInt < 0)
+//                    negativeNumbers.add(numberInt);
+//                else if (numberInt <= 1000)
+//                    returnValue += Integer.parseInt(number.trim());
+//            }
+//        }
+//
+//        if (negativeNumbers.size() > 0) {
+//            throw new RuntimeException("Negative not allowed: " + negativeNumbers.toString());
+//        }
+//
+//        return returnValue;
+//    }
+
+    //Requirement 7
+//    public int add(final String numbers) {
+//        String delimiter = ",|\n";
+//        String numbersUpdated = numbers;
+//
+//        if (numbers.startsWith("//")) {
+//            int beginDelimiterIndex = numbers.indexOf("//") + 2;
+//            int endDelimiterIndex = numbers.indexOf("\n");
+//            if(numbers.charAt(beginDelimiterIndex) == '[' && numbers.charAt(endDelimiterIndex - 1) == ']') {
+//                beginDelimiterIndex += 1;
+//                endDelimiterIndex -= 1;
+//            }
+//            delimiter = numbers.substring(beginDelimiterIndex, endDelimiterIndex);
+//
+//            numbersUpdated = numbers.substring(numbers.indexOf("\n") + 1);
+//        }
+//        return add(numbersUpdated, delimiter);
+//    }
+//
+//    private int add(final String numbers, final String delimiter) {
+//        int returnValue = 0;
+//        List<Integer> negativeNumbers = new ArrayList();
+//        String[] numbersArray = numbers.split(delimiter);
+//        for (String number : numbersArray) {
+//            if (!(number.trim().length() == 0)) {
+//                Integer numberInt = Integer.parseInt(number);
+//                if (numberInt < 0)
+//                    negativeNumbers.add(numberInt);
+//                else if (numberInt <= 1000)
+//                    returnValue += Integer.parseInt(number.trim());
+//            }
+//        }
+//
+//        if (negativeNumbers.size() > 0) {
+//            throw new RuntimeException("Negative not allowed: " + negativeNumbers.toString());
+//        }
+//
+//        return returnValue;
+//    }
+
+    //Requirement 8 - 9
+    public int add(final String numbers) {
+        //default delimiter
         String delimiter = ",|\n";
-        String numbersWithoutDelimiter = numbers;
+        String numbersUpdated = numbers;
+        String[] delimiters;
+
+        //if it begin by '//' then it's a personalize delimiter(s)
         if (numbers.startsWith("//")) {
-            int delimiterIndex = numbers.indexOf("//") + 2;
-            delimiter = numbers.substring(delimiterIndex, delimiterIndex + 1);
-            numbersWithoutDelimiter = numbers.substring(numbers.indexOf("\n") + 1);
+            int beginDelimiterIndex = numbers.indexOf("//") + 2;
+            int endDelimiterIndex = numbers.indexOf("\n");
+            //if it begin with '[' and end by ']' there are multiple delimiters
+            if(numbers.charAt(beginDelimiterIndex) == '[' && numbers.charAt(endDelimiterIndex - 1) == ']') {
+                //use an Apache library to get each delimiters between '[' and ']'
+                delimiters = StringUtils.substringsBetween(numbers.substring(beginDelimiterIndex, endDelimiterIndex), "[", "]");
+                delimiter = "";
+                //iterate through the delimiters list and concatenate the delimiter with a '|' between
+                //them so that the split method will be OK with the regex
+                for (String element: delimiters) {
+                    delimiter += element;
+                    delimiter += "|";
+                }
+                delimiter = StringUtils.removeEnd(delimiter, "|");
+            } else {
+                //there is only one delimiter
+                delimiter = numbers.substring(beginDelimiterIndex, endDelimiterIndex);
+            }
+
+            //"cut" the beginning of the string so that the personalize delimiter is removed
+            numbersUpdated = numbers.substring(numbers.indexOf("\n") + 1);
         }
-        return add(numbersWithoutDelimiter, delimiter);
+        return add(numbersUpdated, delimiter);
     }
 
-    private static int add(final String numbers, final String delimiter) {
+    private int add(final String numbers, final String delimiter) {
         int returnValue = 0;
         List<Integer> negativeNumbers = new ArrayList();
+
+        //a string tab containing the numbers without the delimiter(s) is created
         String[] numbersArray = numbers.split(delimiter);
+
         for (String number : numbersArray) {
             if (!(number.trim().length() == 0)) {
                 Integer numberInt = Integer.parseInt(number);
@@ -161,5 +254,4 @@ public class StringCalculator {
 
         return returnValue;
     }
-
 }
--- src/test/java/ch/hearc/ig/odi/stringcalculatortdd/StringCalculatorTest.java
+++ src/test/java/ch/hearc/ig/odi/stringcalculatortdd/StringCalculatorTest.java
@@ -1,73 +1,76 @@
 package ch.hearc.ig.odi.stringcalculatortdd;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class StringCalculatorTest {
 
-    //Requirement 1
+    private StringCalculator sc;
 
-    //this test is deleted at requirement 4
-//    @Test(expected = RuntimeException.class)
-//    public void whenMoreThan2NumbersAreUsedThenExceptionIsThrown() {
-//        StringCalculator.add("1,2,3");
+    //maybe to delete
+//    @Test
+//    public void when2NumbersAreUsedThenNoExceptionIsThrown() {
+//        StringCalculator.add("1,2");
+//        Assert.assertTrue("When 2 numbers are used, there is no exception", true);
 //    }
-    @Test
-    public void when2NumbersAreUsedThenNoExceptionIsThrown() {
-        StringCalculator.add("1,2");
-        Assert.assertTrue("When 2 numbers are used, there is no exception", true);
-    }
-    @Test(expected = RuntimeException.class)
-    public void whenNonNumberIsUsedThenExceptionIsThrown() {
-        StringCalculator.add("1,X");
+//    @Test(expected = RuntimeException.class)
+//    public void whenNonNumberIsUsedThenExceptionIsThrown() {
+//        StringCalculator.add("1,X");
+//    }
+
+    @Before
+    public void setup() {
+        this.sc = new StringCalculator();
     }
 
-    //Requirement 2
+    //Requirement 1.1
     @Test
     public void whenStringIsEmptyThenZero() {
-        Assert.assertEquals("When the string is empty then zero is returned", 0, StringCalculator.add(""));
+        Assert.assertEquals("When the string is empty then zero is returned", 0, this.sc.add(""));
+    }
+
+    // Requirement 1.2
+    @Test
+    public void whenOneNumberIsUsedThenReturnValueIsThatSameNumber() {
+        Assert.assertEquals("When a single number is used, the value returned is the number himself",4, this.sc.add("4"));
+    }
+
+    // Requirement 1.3
+    @Test
+    public void whenTwoNumbersAreUsedThenReturnValueIsTheirSum() {
+        Assert.assertEquals("When two number are used, the value returned is their sum", 7, this.sc.add("3,4"));
+    }
+
+    // Requirement 2
+    @Test
+    public void whenAnyNumberOfNumbersIsUsedThenReturnValuesAreTheirSums() {
+        Assert.assertEquals("When multiple numbers are used, the value returned is their sum", 3+6+15+18+46+33, this.sc.add("3,6,15,18,46,33"));
     }
 
     // Requirement 3
     @Test
-    public void whenOneNumberIsUsedThenReturnValueIsThatSameNumber() {
-        Assert.assertEquals("When a single number is used, the value returned is the number himself",4, StringCalculator.add("4"));
-    }
-
-    @Test
-    public void whenTwoNumbersAreUsedThenReturnValueIsTheirSum() {
-        Assert.assertEquals("When two number are used, the value returned is their sum", 7, StringCalculator.add("3,4"));
+    public void whenNewLineIsUsedBetweenNumbersThenReturnValuesAreTheirSums() {
+        Assert.assertEquals("When a new line is used between numbers, the value returned is their sum", 3+7+8, this.sc.add("3,7\n8"));
     }
 
     // Requirement 4
     @Test
-    public void whenAnyNumberOfNumbersIsUsedThenReturnValuesAreTheirSums() {
-        Assert.assertEquals("When multiple numbers are used, the value returned is their sum", 3+6+15+18+46+33, StringCalculator.add("3,6,15,18,46,33"));
+    public void whenDelimiterIsSpecifiedThenItIsUsedToSeparateNumbers() {
+        Assert.assertEquals("When a delimiter is specified, it is used to separate the numbers," +
+                "the value returned is their sum",9+5+2, this.sc.add("//;\n9;5;2"));
     }
 
     // Requirement 5
-    @Test
-    public void whenNewLineIsUsedBetweenNumbersThenReturnValuesAreTheirSums() {
-        Assert.assertEquals("When a new line is used between numbers, the value returned is their sum", 3+7+8, StringCalculator.add("3,7\n8"));
-    }
-
-    // Requirement 6
-    @Test
-    public void whenDelimiterIsSpecifiedThenItIsUsedToSeparateNumbers() {
-        Assert.assertEquals("When a delimiter is specified, it is used to separate the numbers," +
-                "the value returned is their sum",9+5+2, StringCalculator.add("//;\n9;5;2"));
-    }
-
-    // Requirement 7
     @Test(expected = RuntimeException.class)
     public void whenNegativeNumberIsUsedThenRuntimeExceptionIsThrown() {
-        StringCalculator.add("9,3,-2,3");
+        this.sc.add("9,3,-2,3");
     }
     @Test
     public void whenNegativeNumbersAreUsedThenRuntimeExceptionIsThrown() {
         RuntimeException exception = null;
         try {
-            StringCalculator.add("3,4,-2,3,-5,2,-1");
+            this.sc.add("3,4,-2,3,-5,2,-1");
         } catch (RuntimeException e) {
             exception = e;
         }
@@ -75,10 +78,31 @@ public class StringCalculatorTest {
                 "all the negative numbers", "Negative not allowed: [-2, -5, -1]", exception.getMessage());
     }
 
-    // Requirement 8
+    // Requirement 6
     @Test
     public void whenOneOrMoreNumbersAreGreaterThan1000IsUsedThenItIsNotIncludedInSum() {
         Assert.assertEquals("When one or more number are greater than 1000, they are not included" +
-                "in the sum", 1003, StringCalculator.add("1000,1,1001,2,1203"));
+                "in the sum", 1003, this.sc.add("1000,1,1001,2,1203"));
+    }
+
+    // Requirement 7
+    @Test
+    public void whenDelimiterIsGreaterThan1CharacterThenItIsUsedToSeparateNumbers() {
+        Assert.assertEquals("When a delimiter is greater than one character, then it is use to separate numbers",
+                7, this.sc.add("//[\\*\\*\\*]\n3***4"));
+    }
+
+    // Requirement 8
+    @Test
+    public void whenMultipleDelimitersAreUsedThenTheyAreUsedToSeparateNumbers() {
+        Assert.assertEquals("When multiple delimiters are used, then they are used to separate the numbers",
+                10, this.sc.add("//[\\*][:][,]\n1,3*2:4"));
+    }
+
+    // Requirement 9
+    @Test
+    public void whenMultipleDelimitersOfMultipleCharactersAreUsedThenTheyAreUsedToSeparateNumbers() {
+        Assert.assertEquals("When multiple delimiters of multiple characters are used, then they are used to separate the numbers",
+                10, this.sc.add("//[\\*\\*\\*\\*][::::][,,,,]\n1,,,,3****2::::4"));
     }
 }
